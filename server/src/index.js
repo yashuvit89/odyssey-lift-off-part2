de0ad06689bb455a2b0c4348ac11561f4b7eb94f
Yaswanth Pedasanganti <ypedasanganti@atlassian>
2021-05-31 00:02:56 +0530

    Link datasources, resolvers in Apollo graphQL server

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,16 @@
-const { ApolloServer, MockList } = require('apollo-server');
-const typeDefs = require('./schema');
-
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => new MockList([6, 9]),
-  }),
-  Track: () => ({
-    id: () => 'track_01',
-    title: () => 'Astro Kitty, Space Explorer',
-    author: () => {
-      return {
-        name: 'Grumpy Cat',
-        photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
-      };
-    },
-    thumbnail: () =>
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const TrackAPI = require("./datasources/track-api");
+const resolvers = require("./resolvers");
 
 const server = new ApolloServer({
   typeDefs,
-  mocks,
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    };
+  },
 });
 
 server.listen().then(() => {
